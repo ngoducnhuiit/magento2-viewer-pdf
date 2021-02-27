3f149d5b56acd52b7348895aacbdb03b1d6f0014
ngoducnhuiit <ngoducnhuiit@gmail.com>
2021-02-27 12:06:17 +0700

    edit requijs

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -5,16 +5,18 @@
 var config = {
     paths: {
         'pdfjs-dist': 'Magepow_ViewerPdf/js/pdfjs-dist',
+        'pdfjs-dist/web/debugger': 'Magepow_ViewerPdf/js/pdfjs-dist/js/web/debugger',
         'pdfjs-dist/build/pdf': 'Magepow_ViewerPdf/js/pdfjs-dist/build/pdf',
         'pdfjs-dist/build/pdf.worker': 'Magepow_ViewerPdf/js/pdfjs-dist/build/pdf.worker',
-        'pdfjs-dist/web/viewer': 'Magepow_ViewerPdf/js/web/viewer'   
+        'pdfjs-dist/web/viewer': 'Magepow_ViewerPdf/js/web/viewer',
+        'pdfjs-config': 'Magepow_ViewerPdf/js/web/pdfjs-config'   
     },
-    // shim: {
-    //     'pdfjs-config': {
-    //         'deps': ['pdfjs-dist/build/pdf', 'pdfjs-dist/build/pdf.worker']
-    //     },
-    //     'pdfjs-dist/web/viewer': {
-    //         'deps': ['pdfjs-config']
-    //     }
-    // }
+    shim: {
+        'pdfjs-config': {
+            'deps': ['pdfjs-dist/build/pdf', 'pdfjs-dist/build/pdf.worker']
+        },
+        'pdfjs-dist/web/viewer': {
+            'deps': ['pdfjs-config']
+        }
+    }
 };
---
+++ view/frontend/web/js/web/pdfjs-config.js
@@ -0,0 +1,4 @@
+define(['pdfjs-dist/build/pdf'], function(PDF) {
+    window['pdfjs-dist/build/pdf'] = PDF;
+    return PDF;
+});
